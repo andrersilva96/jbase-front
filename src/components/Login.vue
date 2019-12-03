a5bbe5003ca593c332b96424de6f8ebc313ae0a5
Andre Rodrigues <andre.rsilva96@hotmail.com>
2019-12-02 23:16:02 -0300

    Simple login with Google Facebook and Github on firebase

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,31 @@
 <template>
-  <span>Login</span>
+  <div>
+    <span>Login</span><br>
+    <a href="#" @click="login($event)" data-provider="Google">Sign in with Google</a><br>
+    <a href="#" @click="login($event)" data-provider="Facebook">Sign in with Facebook</a><br>
+    <a href="#" @click="login($event)" data-provider="Github">Sign in with Github</a><br>
+  </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  methods: {
+    async login(event) {
+      let provider
+      switch (event.target.getAttribute('data-provider')) {
+        case 'Google': provider = new firebase.auth.GoogleAuthProvider()
+        case 'Facebook': provider = new firebase.auth.FacebookAuthProvider()
+        case 'Github': provider = new firebase.auth.GithubAuthProvider()
+      }
+      provider.setCustomParameters({prompt: 'select_account'})
+      let result = await firebase.auth().signInWithPopup(provider)
+      let idToken = await result.user.getIdToken()
+      console.log(idToken)
+    }
+  }
 }
 </script>
 
