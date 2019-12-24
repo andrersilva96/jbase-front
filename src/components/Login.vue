972638a5e36d7d0994c893de49e2bb9f798a8170
Andre Rodrigues <andre.rsilva96@hotmail.com>
2019-12-23 22:45:26 -0300

    Redir to dashboard

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,21 +9,33 @@
 
 <script>
 import firebase from 'firebase'
+import axios from 'axios'
+import JwtTokenService from '../services/JwtToken'
 
 export default {
   name: 'Login',
   methods: {
     async login(event) {
-      let provider
-      switch (event.target.getAttribute('data-provider')) {
-        case 'Google': provider = new firebase.auth.GoogleAuthProvider()
-        case 'Facebook': provider = new firebase.auth.FacebookAuthProvider()
-        case 'Github': provider = new firebase.auth.GithubAuthProvider()
-      }
-      provider.setCustomParameters({prompt: 'select_account'})
-      let result = await firebase.auth().signInWithPopup(provider)
-      let idToken = await result.user.getIdToken()
-      console.log(idToken)
+      try {
+        let provider
+        switch (event.target.getAttribute('data-provider')) {
+          case 'Google': provider = new firebase.auth.GoogleAuthProvider()
+          case 'Facebook': provider = new firebase.auth.FacebookAuthProvider()
+          case 'Github': provider = new firebase.auth.GithubAuthProvider()
+        }
+        provider.setCustomParameters({prompt: 'select_account'})
+        let result = await firebase.auth().signInWithPopup(provider)
+        let req = await axios({
+          method: 'post',
+          url: process.env.API_DOMAIN + '/login',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': await result.user.getIdToken()
+          }
+        })
+        JwtTokenService.setToken(req.data.token)
+        this.$router.push('dashboard')
+      } catch (err) {}
     }
   }
 }
